59496cd704c688888e61c5cc84b1f147d432c3dc
ale0Esp <ale_1617@outlook.com>
2023-01-07 15:38:51 -0600

    tecno thymeleaf

---
+++ HelloSpringThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
@@ -0,0 +1,16 @@
+package mx.com.gm;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller//Controlador MVC
+@Slf4j
+public class ControladorInicio {
+    @GetMapping("/")
+    public String inicio(){
+        log.info("Ejecuntando el controlador SpringMVC");
+        return "index";
+    }
+    
+}
